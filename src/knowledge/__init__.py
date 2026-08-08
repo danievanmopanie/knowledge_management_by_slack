9e4ddce0408454ab38100ab59e210615ab6e8d89
Danie Ungerer <39651281+danievanmopanie@users.noreply.github.com>
2026-08-08 10:27:46 +0200

    Export file_loader helpers

--- src/knowledge/__init__.py
+++ src/knowledge/__init__.py
@@ -1,5 +1,6 @@
 """Hybrid RAG layer: Vector store + lightweight Knowledge Graph."""
 
+from .file_loader import download_slack_file, extract_text
 from .graphstore import GraphStore
 from .ingest import ingest_file, ingest_text
 from .retriever import HybridRetriever, RetrievalResult
@@ -12,4 +13,6 @@ __all__ = [
     "RetrievalResult",
     "ingest_text",
     "ingest_file",
+    "download_slack_file",
+    "extract_text",
 ]
